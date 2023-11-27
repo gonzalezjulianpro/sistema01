94cb49da9cfc4d5c5d841ab2a5a446310d87325b
gonzalezjulianpro <julian@MacBook-Pro-de-Julian.local>
2023-11-27 19:29:51 -0300

    Conclusao funcao cadastro_nova_pessoa() (macbook).

--- cadastro_nova_pessoa.py
+++ cadastro_nova_pessoa.py
@@ -22,21 +22,21 @@ def cadastro_pessoa_main():  #Fucao principal de chamadas
 
 def criar_lista():#Criando arquivo de banco de dados txt
 
-    print("\nCriacao Banco de Dados TXT")
+    print("\nCriacao Banco de Dados CSV")
     banco_dado = input("Insira o nome do banco de dados para criacao, com 'BD' no final.\nEx: bancoBD\nNome: ")
     banco_dado = banco_dado.strip()
-    print("Nome do Banco de Dados: {}.txt".format(banco_dado))
+    print("Nome do Banco de Dados: {}.csv".format(banco_dado))
     confirmacao = input("1 - O nome esta correto\n2 - O nome esta incorreto\nOpcao: ")#Confirmacao para criacao de banco
     confirmacao = int(confirmacao.strip())
     if(confirmacao == 1):
-        arquivo = open("{}.txt".format(banco_dado), "w") #comando para criar o arquivo .txt com o nome escolhido pelo usuario
+        arquivo = open("{}.csv".format(banco_dado), "w") #comando para criar o arquivo .txt com o nome escolhido pelo usuario
         arquivo.close()
         cadastro_pessoa_main()
     else:
         confirmacao2 = input("1 - Tentar Novamente\n2 - Voltar ao Menu Anterior\nOpcao: ")#Confirmacao para nova criacao de banco, tentar novamente?
         confirmacao2 = int(confirmacao2.strip())
         if(confirmacao2 == 1):
-            print("!!!Nova Tentativa de Criacao de Banco de Dados TXT!!!")
+            print("!!!Nova Tentativa de Criacao de Banco de Dados CSV!!!")
             criar_lista()
         elif(confirmacao2 == 2):
             print("!!!Retornando ao Menu anterior!!!")
@@ -49,17 +49,47 @@ def adiciona():#funcao de adicao de pessoa no sistema
 
     diretorio = input("Insira o caminho completo do diretorio utilizado: ").strip()
     chama_banco = input("Insira o nome do banco desejado: ").strip()
-    teste_mensagem = input("Escreve mensagem de teste: \n") #Testando escrita em arquivo
-    with open("{}{}.txt".format(diretorio, chama_banco), "a") as escreve_banco:
-        escreve_banco.write("\n{}".format(teste_mensagem))
-        escreve_banco.close()
-    cadastro_pessoa_main()
+    menu.pulaLinha()
+    print("Banco Escolhido: {}{}.csv".format(diretorio, chama_banco))
+    confirma = int(input("1 - O banco esta correto.\n2 - O banco esta incorreto\nOpcao: "))# primeira confirmacao necessaria
+
+    if (confirma == 1):
+        print("*** Novo Cadastro ***")
+        nome = input("Nome: ")
+        idade = input("Idade: ")
+        profissao = input("Profissao: ")
+        status_civil = input("Status Civil: ")
+        dados = "{},{},{},{}".format(nome, idade, profissao, status_civil)
+        dados = dados.strip()
+        with open("{}{}.csv".format(diretorio, chama_banco), "a") as escreve_banco: # Escrevendo no arquivo
+            escreve_banco.write("\n{}".format(dados))
+            escreve_banco.close()
+        cadastro_pessoa_main()
+    elif (confirma == 2):
+        confirma2 = int(input("1 - Tentar novamente.\n2 - Cancelar e retornar ao Menu anterior\nOpcao: "))# Segunda confirmacao necessaria
+        if (confirma2 == 1):
+            print("!!!Nova tentativa!!!")
+            menu.pulaLinha()
+            adiciona()
+        elif (confirma2 == 2):
+            print("Cancelando registro. Retornando ao Menu anterior!")
+            menu.pulaLinha()
+            cadastro_pessoa_main()
+        else:
+            print("Opcao invalida. Retornando ao Menu Principal.")
+            menu.pulaLinha()
+            menu.iniciando()
+    else:
+        print("Opcao invalida. Retornando ao Menu anterior!")
+        menu.pulaLinha()
+        cadastro_pessoa_main()
+
 
 
 def confere_listas():#Lista todos os bancos que já foram criados
 
     diretorio = input("Insira o caminho completo do diretorio utilizado: ").strip()
-    file_end = "BD.txt"
+    file_end = "BD.csv"
     menu.pulaLinha()
     print("Bancos:")
     for file in os.listdir(diretorio):
