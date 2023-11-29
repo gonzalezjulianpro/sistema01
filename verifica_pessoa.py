e5ff67274b8e05d2352fb685b3addaad7145188f
gonzalezjulianpro <julian@MacBook-Pro-de-Julian.local>
2023-11-28 22:44:35 -0300

    iniciando funcao verifica_pessoa() manipulacao de CSV com PANDAS (macbook).

--- verifica_pessoa.py
+++ verifica_pessoa.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 import cadastro_nova_pessoa
 import menu
 
@@ -23,14 +24,26 @@ def verifica_pessoa_main():
         verifica_pessoa_main()
 
 
+'''
 def todos_cadastros():# leitura de banco csv
     banco = input("Passe o caminho completo do banco: ").strip()
     with open("{}".format(banco), "r") as mostra_banco: #abertura do arquivo csv em modo de leitura
         dado = csv.reader(mostra_banco)
         for linha in dado: #impressao linha por linha do arquivo csv
+
             print(linha)
     mostra_banco.close()
-
+'''
+def todos_cadastros():# leitura de banco csv
+    caminho = input("Passe o caminho completo do banco: ").strip()
+    menu.pulaLinha()
+    banco = pd.read_csv(caminho) #abertura do arquivo csv em modo de leitura
+    banco.columns = ["Nome", "Idade", "Profissao", "Status"] #criando colunas para exibicoa do arquivo de banco csv
+    print(banco)
+    menu.pulaLinha()
+    print("!!!Retornando ao Menu anterior!!!")
+    menu.pulaLinha()
+    verifica_pessoa_main()
 
 
 if(__name__ == "__main__"):
